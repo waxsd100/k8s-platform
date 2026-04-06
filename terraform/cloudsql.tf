df7fa8f7916fe49eabd37aa16c592c695582685e
waxsd100 <12383342+waxsd100@users.noreply.github.com>
2026-04-06 23:17:22 +0900

    feat: add Terraform configuration for Cloud SQL instance provisioning

--- terraform/cloudsql.tf
+++ terraform/cloudsql.tf
@@ -16,7 +16,10 @@ resource "google_compute_global_address" "private_ip_range" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.vpc_network.id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
+  reserved_peering_ranges = [
+    "wax100-vpc-ip-range",
+    google_compute_global_address.private_ip_range.name
+  ]
 
   depends_on = [google_project_service.enabled_apis]
 }
